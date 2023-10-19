285ac1908ff7d262102d529d57364e373d8355c1
Alethepata <alessia.patane@gmail.com>
2023-10-19 16:58:08 +0200

    [Add] menu

---
+++ src/data/menu.js
@@ -0,0 +1,37 @@
+export default [
+    {
+        title: 'Home',
+        link: '#'
+
+    },
+    {
+        title: 'About Us',
+        link: '#'
+
+    },
+    {
+        title: 'Portfolio',
+        link: '#'
+
+    },
+    {
+        title: 'Our Process',
+        link: '#'
+
+    },
+    {
+        title: 'Pricing',
+        link: '#'
+
+    },
+    {
+        title: 'Blog',
+        link: '#'
+
+    },
+    {
+        title: 'Contact',
+        link: '#'
+
+    }
+]
