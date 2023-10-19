e371b9544a1dec057d61fc6d898cf65bcdb1d0aa
Alethepata <alessia.patane@gmail.com>
2023-10-19 18:03:14 +0200

    [Add] li

---
+++ src/components/partials/MenuList.vue
@@ -0,0 +1,27 @@
+<script>
+export default {
+    name: 'MenuList',
+    props: {
+        title: String,
+        link: String
+    }
+}
+</script>
+
+<template>
+    <li>
+        <a :href="link">
+            {{ title }}
+        </a>
+    </li>
+</template>
+
+<style lang="scss">
+@use '../../scss/partials/var' as*;
+
+a:hover{
+    color: $orange;
+}
+
+
+</style>
