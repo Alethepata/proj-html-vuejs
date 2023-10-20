28b4b0f1cf199798fd60b7ef31dd7873ae5c5e13
Alethepata <alessia.patane@gmail.com>
2023-10-20 15:50:08 +0200

    [Add] getImg( )

--- src/components/partials/Design.vue
+++ src/components/partials/Design.vue
@@ -1,6 +1,11 @@
 <script>
 export default {
-    name:'Design'
+    name: 'Design',
+    methods: {
+        getImg(img) {
+            return new URL (`../../assets/${img}.png`, import.meta.url).href
+        }
+    }
 
 }
 </script>
@@ -10,20 +15,20 @@ export default {
   <div class="d-flex justify-content-between position-relative size">
 
     <div class="center position-absolute">
-        <img src="../../assets/globe3.png" alt="">
+        <img :src="getImg('globe3')" alt="">
     </div>
 
     <div class="left position-relative">
-        <img class="dots dots-left" src="../../assets/dots-left.png" alt="">        
-        <img class="globe-one" src="../../assets/globe1.png" alt="">
-        <img class="man position-absolute bottom-0" src="../../assets/left-man.png" alt="">
+        <img class="dots dots-left" :src="getImg('dots-left')" alt="">        
+        <img class="globe-one" :src="getImg('globe1')" alt="">
+        <img class="man position-absolute bottom-0" :src="getImg('left-man')" alt="">
 
     </div>
     <div class="right text-end "> 
-        <img class="dots dots-right" src="../../assets/dots-right.png" alt="">        
-        <img class="ligth" src="../../assets/light.png" alt="">     
-        <img class="globe" src="../../assets/globe2.png" alt="">     
-        <img class="man" src="../../assets/right-man.png" alt="">
+        <img class="dots dots-right" :src="getImg('dots-right')" alt="">        
+        <img class="light" :src="getImg('light')" alt="">     
+        <img class="globe" :src="getImg('globe2')" alt="">     
+        <img class="man" :src="getImg('right-man')" alt="">
   
     </div>
   </div>
@@ -65,7 +70,7 @@ export default {
     padding-right: 400px;
     height: 100px;
 }
-.ligth{
+.light{
     padding-right: 130px;
 }
 </style>
