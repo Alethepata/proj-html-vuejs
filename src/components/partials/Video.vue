8179aa12c2bf698f79a806067183380af49d6a5b
Alethepata <alessia.patane@gmail.com>
2023-10-20 17:00:49 +0200

    [Add] img/ i

---
+++ src/components/partials/Video.vue
@@ -0,0 +1,36 @@
+<script>
+export default {
+    name: 'Video',
+    methods: {
+        getImg(img) {
+            return new URL(`../../assets/${img}.png`, import.meta.url).href
+        }
+    }
+}
+</script>
+
+<template>
+  <div class="container_custom">
+    <div class="video position-relative">
+        <img class="w-100 object-fit-cover " :src="getImg('middle')" alt="">
+        <i class="fa-solid fa-play play rounded-circle"></i>
+    </div>
+  </div>
+</template>
+
+<style lang="scss">
+@use '../../scss/partials/var' as *;
+.play{
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%,-50%);
+    color: $white;
+    font-size: 30px;
+    background-color: $magenta;
+    text-align: center;
+    line-height: 100px;
+    height: 100px;
+    width: 100px;
+}
+</style>
