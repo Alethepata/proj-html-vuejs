cbb0ad8e5cb711b3080ac5dc2073dccccb29799f
Alethepata <alessia.patane@gmail.com>
2023-10-21 16:49:03 +0200

    [Edit] position-relative

--- src/components/partials/FirstSection.vue
+++ src/components/partials/FirstSection.vue
@@ -10,97 +10,87 @@ export default {
 </script>
 
 <template>
-  <section>
+  <section class="d-flex position-relative">
+    
+    <img class="image one" :src="getImg('smart')" alt=""> 
 
-    <div class="position_relative">
-        <img class="image one" :src="getImg('smart')" alt="">
-        
-        <div class="container_custom">
+    <div class="container_custom">
 
-            <div class="text text-center w-75 ">
+        <div class="text text-center w-75 ">
 
-                <h3>our love for animation</h3>
-                <h1>we're storytelling expert with creative smarts</h1>
-                <p>
-                    Our culture inspiries the creative and inovation spirit found throughout our Studio. We breathe life into new ideas!
-                </p>
-
-            </div>
-
-            <div class="cards d-flex justify-content-center mt-5">
-
-                <div class="card rounded-5">
-
-                    <div class="card-image">
-
-                        <img src="../../assets/smart1.png" alt="">
-
-                    </div>
-
-                    <div class="card-text">
-
-                        <h3>3D modelling</h3>
-                        <p>
-                            We constantly ask questions. It helps us shape your story and deliver the results you creave. Yep, we'll drill deep to deliver above and beyond your expectations.
-                        </p>
-
-                    </div>
-
-                </div>
-                <div class="card rounded-5 mx-4">
-
-                    <div class="card-image">
-
-                        <img src="../../assets/smart2.png" alt="">
-
-                    </div>
-
-                    <div class="card-text">
-
-                        <h3>3D modelling</h3>
-                        <p>
-                            We constantly ask questions. It helps us shape your story and deliver the results you creave. Yep, we'll drill deep to deliver above and beyond your expectations.
-
-                        </p>
-
-                    </div>
-
-                </div>
-
-                <div class="card rounded-5">
-
-                    <div class="card-image">
-
-                        <img src="../../assets/smart3.png" alt="">
-
-                    </div>
-
-                    <div class="card-text">
-
-                        <h3>3D modelling</h3>
-                        <p>
-                            We constantly ask questions. It helps us shape your story and deliver the results you creave. Yep, we'll drill deep to deliver above and beyond your expectations.
-                        </p>
-
-                   </div>
-
-                </div>
-
-            </div>
-
-            <img class="image two" :src="getImg('smart4')" alt="">
+            <h3>our love for animation</h3>
+            <h1>we're storytelling expert with creative smarts</h1>
+            <p>
+                Our culture inspiries the creative and inovation spirit found throughout our Studio. We breathe life into new ideas!
+            </p>
 
         </div>
 
-    </div>
+        <div class="cards d-flex justify-content-center mt-5">
 
+            <div class="card rounded-5">
+
+                <div class="card-image">
+                    <img src="../../assets/smart1.png" alt="">
+                </div>
+
+                <div class="card-text">
+
+                    <h3>3D modelling</h3>
+                    <p>
+                        We constantly ask questions. It helps us shape your story and deliver the results you creave. Yep, we'll drill deep to deliver above and beyond your expectations.
+                    </p>
+
+                </div>
+
+            </div>
+
+            <div class="card rounded-5 mx-4">
+
+                <div class="card-image">
+                    <img src="../../assets/smart2.png" alt="">
+                </div>
+
+                <div class="card-text">
+
+                    <h3>3D modelling</h3>
+                    <p>
+                        We constantly ask questions. It helps us shape your story and deliver the results you creave. Yep, we'll drill deep to deliver above and beyond your expectations.
+                    </p>
+
+                </div>
+
+            </div>
+
+            <div class="card rounded-5">
+
+                <div class="card-image">
+                    <img src="../../assets/smart3.png" alt="">
+                </div>
+
+                <div class="card-text">
+
+                    <h3>3D modelling</h3>
+                    <p>
+                        We constantly ask questions. It helps us shape your story and deliver the results you creave. Yep, we'll drill deep to deliver above and beyond your expectations.
+                    </p>
+
+               </div>
+
+            </div>
+
+        </div>
+
+        <img class="image two" :src="getImg('smart4')" alt="">
+
+    </div>
   </section>
 </template>
 
 <style lang="scss" scoped>
 @use '../../scss/partials/var' as*;
 .image{
-        position: absolute;
+        
         
 }
 .image.one{
