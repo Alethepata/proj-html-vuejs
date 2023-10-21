01ad401123cdd9418f4674032fc5eb1c98c7623e
Alethepata <alessia.patane@gmail.com>
2023-10-21 18:03:50 +0200

    [Add] text/ img/ button

--- src/components/partials/ThirdSection.vue
+++ src/components/partials/ThirdSection.vue
@@ -11,12 +11,43 @@ export default {
 
 <template>
   <section>
-    
+    <div class="container_custom text-center">
+        <div class="row">
+            <div class="col">
+                <h3>our works</h3>
+                <h1>featured productions</h1>
+                <p>
+                    Here's just a small sample of some of those projects that we're proud of.
+                    If you're looking for something specific feel to get in conctact with us
+                </p>
+            </div>
+        </div>
+        <div class="row row-cols-2">
+            <div class="col mb-4">
+                <img :src="getImg('work1')" alt="">
+            </div>
+            <div class="col">
+                <img :src="getImg('work2')" alt="">
+            </div>
+            <div class="col">
+                <img :src="getImg('work3')" alt="">
+            </div>
+            <div class="col">
+                <img :src="getImg('work4')" alt="">
+            </div>
+        </div>
+        <div class="row mt-5">
+            <div class="col">
+                <button>see our work</button>
+            </div>
+        </div>
+    </div>
   </section>
 </template>
 
 <style lang="scss" scoped>
 @use '../../scss/partials/var' as*;
-
-
+img{
+    width: 100%;
+}
 </style>
