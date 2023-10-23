b3c0cb1b8767b1a0ba399b3700a5223ecb4cb75d
Alethepata <alessia.patane@gmail.com>
2023-10-23 17:29:25 +0200

    [Add] footer menu

---
+++ src/data/menuFooter.js
@@ -0,0 +1,27 @@
+export default [
+    {
+        title: 'About Us',
+        link: '#'
+
+    },
+    {
+        title: 'Portfolio',
+        link: '#'
+
+    },
+    {
+        title: 'Pricing',
+        link: '#'
+
+    },
+    {
+        title: 'Career',
+        link: '#'
+
+    },
+    {
+        title: 'Contact',
+        link: '#'
+
+    }
+]
