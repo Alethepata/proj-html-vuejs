3ca7e3bfcf29ced6e4fe7c3222122cfce4ec7043
Alethepata <alessia.patane@gmail.com>
2023-10-23 11:31:16 +0200

    [Add] text/cards/arrows

--- src/components/partials/FifthSection.vue
+++ src/components/partials/FifthSection.vue
@@ -1,12 +1,80 @@
 <script>
 export default {
-    name:'FifthSection'
+    name:'FifthSection',
+    methods: {
+        getImg(img) {
+            return new URL (`../../assets/${img}.png`, import.meta.url).href
+        }
+    }
 }
 </script>
 
 <template>
   <section>
-    
+    <div class="container_custom text-center">
+        <div class="text">
+            <h3>andidio magic team member</h3>
+            <h1>let us animate your project</h1>
+            <p>
+                we create new worlds! Let's collaborate and create engaging, effective award-winning animations
+            </p>
+        </div>
+        <div class="cards">
+            <div class="card">
+                <div class="card-image">
+                    <img :src="getImg('team1')" alt="">
+                </div>
+                <div class="card-text">
+                    <h2>robert coleman</h2>
+                    <p>owner & creative director</p>
+                    <div class="social">
+                        <i class="fa-brands fa-facebook-f"></i>
+                        <i class="fa-brands fa-twitter"></i>
+                        <i class="fa-brands fa-instagram"></i>
+                    </div>
+                </div>
+            </div>
+            <div class="card">
+                <div class="card-image">
+                    <img :src="getImg('team2')" alt="">
+                </div>
+                <div class="card-text">
+                    <h2>don woods</h2>
+                    <p>administrator</p>
+                    <div class="social">
+                        <i class="fa-brands fa-facebook-f"></i>
+                        <i class="fa-brands fa-twitter"></i>
+                        <i class="fa-brands fa-instagram"></i>
+                    </div>
+                </div>
+            </div>
+            <div class="card">
+                <div class="card-image">
+                    <img :src="getImg('team4')" alt="">
+                </div>
+                <div class="card-text">
+                    <h2>tomas nash</h2>
+                    <p>2d animator & compositor</p>
+                    <div class="social">
+                        <i class="fa-brands fa-facebook-f"></i>
+                        <i class="fa-brands fa-twitter"></i>
+                        <i class="fa-brands fa-instagram"></i>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="arrows d-flex justify-content-center ">
+        <i class="fa-solid fa-arrow-left"></i>
+        <div class="circles d-flex justify-content-around">
+            <div class="circle"></div>
+            <div class="circle"></div>
+            <div class="circle"></div>
+            <div class="circle"></div>
+        </div>
+        <i class="fa-solid fa-arrow-right"></i>
+    </div>
+
   </section>
 </template>
 
