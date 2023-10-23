c52d4e3d906f58ceb31188c06f7bec77cfdbe1e8
Alethepata <alessia.patane@gmail.com>
2023-10-23 11:03:31 +0200

    [Add] FifthSection/SixthSection/SeventhSection

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -3,13 +3,19 @@ import FirstSection from './partials/FirstSection.vue';
 import SecondSection from './partials/SecondSection.vue';
 import ThirdSection from './partials/ThirdSection.vue';
 import FourthSection from './partials/FourthSection.vue';
+import FifthSection from './partials/FifthSection.vue';
+import SixthSection from './partials/SixthSection.vue';
+import SeventhSection from './partials/SeventhSection.vue';
 export default {
     name: 'Main',
     components: {
       FirstSection,
       SecondSection,
       ThirdSection,
-      FourthSection
+      FourthSection,
+      FifthSection,
+      SixthSection,
+      SeventhSection
         
     }
 }
