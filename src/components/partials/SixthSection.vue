aba2686e0d2f53a11a830ad1e6040941f3182cb2
Alethepata <alessia.patane@gmail.com>
2023-10-23 12:12:13 +0200

    [Add] text/cards

--- src/components/partials/SixthSection.vue
+++ src/components/partials/SixthSection.vue
@@ -1,15 +1,47 @@
 <script>
 export default {
-    name:'SixthSection'
+  name: 'SixthSection',   
+  methods: {
+        getImg(img) {
+            return new URL (`../../assets/${img}.png`, import.meta.url).href
+        }
+    }
 }
 </script>
 
 <template>
   <section>
-    
+    <div class="container_custom">
+      <div class="col text-center">
+        <h3>why andidio studio</h3>
+        <h1>we love our clients</h1>
+        <p>
+          We are trusted thrpughout adland an have a wealth of recommendations from start-ups to renowed global brands.
+        </p>
+      </div>
+      <div class="cards d-flex justify-content-evenly">
+        <div class="card">
+          <img :src="getImg('sponsor5')" alt="">
+        </div>
+        <div class="card">
+          <img :src="getImg('sponsor6')" alt="">
+        </div>
+        <div class="card">
+          <img :src="getImg('sponsor1')" alt="">
+        </div>
+        <div class="card">
+          <img :src="getImg('sponsor2')" alt="">
+        </div>
+        <div class="card">
+          <img :src="getImg('sponsor3')" alt="">
+        </div>
+      </div>
+    </div>
   </section>
 </template>
 
-<style>
-
+<style lang="scss" scoped>
+.card{
+  border: none;
+}
 </style>
