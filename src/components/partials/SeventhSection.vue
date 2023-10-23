b5f6081f416a9101f0f278eb0f9f253eddf64c12
Alethepata <alessia.patane@gmail.com>
2023-10-23 12:21:43 +0200

    [Add] text/email

--- src/components/partials/SeventhSection.vue
+++ src/components/partials/SeventhSection.vue
@@ -6,10 +6,26 @@ export default {
 
 <template>
   <section>
-    
+    <div class="container_custom text-center py-5">
+      <div class="text">
+        <h3>we create new worlds!</h3>
+        <h1>want tips on your storytellyng?</h1>
+        <p>
+          Join our newsletter for tips on how to elevate your brand through storytellyng
+        </p>
+      </div>
+      <div class="email d-flex">
+        <input type="email" class="form-control" placeholder="Enter you email">
+        <button class="btn_custom_pink">Submit</button>
+      </div>
+    </div>
   </section>
 </template>
 
-<style>
+<style lang="scss" scoped>
+section{
+  background-image: url(../../assets/counter-bg.png);
+  background-position: center;
 
+}
 </style>
